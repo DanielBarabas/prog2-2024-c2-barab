af5fe786fb0e001c71f5825ca0e38016b8dd8e84
DanielBarabas <barabas.daniel1@gmail.com>
2024-03-15 19:00:07 +0100

    first solution ready but slow

--- solution-2/src.py
+++ solution-2/src.py
@@ -1,28 +1,51 @@
 import pandas as pd
+from scipy import spatial
+import pickle
+import numpy as np
 
-if __name__ == "__main__":
 
-    df = pd.read_csv("input.csv")
-    query_df = pd.read_csv("query.csv")
+query_df = pd.read_csv("query.csv")
+meta_df = pd.read_parquet("compute_workfile.parquet")
+with open("dtypes.npy","rb") as file:
+    dtypes = np.load(file,allow_pickle=True)
 
-    damage_types = df["dmg_type"].unique()
-    out = []
+with open("smalltrees.pkl","rb") as file:
+    tree_dict = pickle.load(file)
+
+with open("bigtrees.pkl","rb") as file:
+    tree_dict1 = pickle.load(file)
+
+dmgs = pd.read_parquet("dmgs.parquet")
+
+out_df = pd.DataFrame()
+for dt in dtypes:
+    dmg_list = []
+    temp_dmgs = dmgs.loc[(dmgs["dmg_type"]) == dt,"dmg"].to_numpy()
     for idx, row in query_df.iterrows():
-        out_row = {}
-        for dt in damage_types:
-            sub_df = df.loc[
-                (df["dmg_type"] == dt)
-                & (df["dmg"] >= row["dmg_min"])
-                & (df["dmg"] <= row["dmg_max"]),
-                ["x", "y", "dmg"],
-            ]
-            if sub_df.empty:
-                out_row[dt] = 0
-            else:
-                diffs = ((sub_df[["x", "y"]] - row) ** 2).sum(axis=1)
-                out_row[dt] = sub_df.iloc[diffs.argmin(), 2]
-            #print(out_row)
+        
+        valid_dmgs = meta_df.loc[(meta_df["dmg_type"] == dt)&
+                                    (meta_df["dmg"] >= row["dmg_min"])&
+                                    (meta_df["dmg"]<=row["dmg_max"]),"dmg"]
+        
+        #print(type([valid_dmgs]))
+        if len(valid_dmgs)==0:
+            dmg_list.append(0)
+        elif len(valid_dmgs)>0 and len(valid_dmgs)<20:
 
-        out.append(out_row)
+            dmg_list.append(valid_dmgs.iloc[pd.Series([tree_dict[dt][dmg].query(row[["x","y"]])[0] for
+                dmg in valid_dmgs]).idxmin()])   
+        else:
+            counter = 1
+            while True:
+                    if counter == 1:
+                        index = tree_dict1[dt].query(row[["x","y"]])[1]
+                    else:
+                        index = tree_dict1[dt].query(row[["x","y"]],k=counter)[1][-1]
+                    if (temp_dmgs[index] >= row["dmg_min"]) & (temp_dmgs[index] <= row["dmg_max"]):
+                        dmg_list.append(temp_dmgs[index])
+                        break
+                    else:
+                        counter += 1
+    out_df[dt] = dmg_list    
 
-    pd.DataFrame(out).to_csv("out.csv", index=False)
+out_df.to_csv("out.csv",index= False)
