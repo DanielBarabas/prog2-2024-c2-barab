dfc13ee9e28a09a87a2dbee2bd2c40f1ad1e7b57
DanielBarabas <barabas.daniel1@gmail.com>
2024-03-17 22:27:38 +0100

    port 8000

--- barab-virag-3/comm.sh
+++ barab-virag-3/comm.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-screen -dmS flask_server python3 preproc.py; while ! nc -z localhost 5678; do sleep 1; done;
+screen -dmS flask_server python3 preproc.py; while ! nc -z localhost 8000; do sleep 1; done;
--- barab-virag-3/preproc.py
+++ barab-virag-3/preproc.py
@@ -76,4 +76,4 @@ def ok():
 
 
 if __name__ == "__main__":
-    app.run(port=5678)
+    app.run(port=8000)
